29df1e0e8a2a962be1af6e5907e58b40ef4038eb
Harsh2220 <harshgajjar145@gmail.com>
2022-04-11 20:25:47 +0530

    team

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <title>CyberJunk</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
